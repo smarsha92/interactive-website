32ee6823fe4df6b6a80487d5ea9db94d638d2bb3
smarsha92 <70488663+smarsha92@users.noreply.github.com>
2025-12-09 16:36:36 +0000

    Edited Spark

--- src/not-found.tsx
+++ src/not-found.tsx
@@ -1,11 +1,11 @@
 export default function NotFound() {
-  return (
-    <div style={{ textAlign: 'center', padding: '50px' }}>
-      <h1>404 - Page Not Found</h1>
+    <div s
       <p>The page you are looking for does not exist.</p>
-      <a href="/">
         Go back to Home
-      </a>
+    </div>
+}
+
+
     </div>
   );
 }
